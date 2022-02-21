03cc4b9510f3ad54f04f7e417cf05d76d8d0f9e6
Horst-Christian Heinke <98899060+east-code@users.noreply.github.com>
2022-02-21 13:48:02 +0100

    Add files via upload

---
+++ 12_Klassen/ClassBubblesort/Form_Bubblesort.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bubblesort
+{
+public partial class Form_Bubblesort : Form
+{
+    public Form_Bubblesort()
+    {
+        InitializeComponent();
+    }
+    #region Variablen
+    const int n = 20;
+    int[] X = new int[n];
+    #endregion Variablen
+    /// <summary>
+    /// Generiert 20 Zufallszahlen zwische 0 und 100
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void button_zufallszahlen_Click(object sender, EventArgs e)
+    {
+        Random rnd = new Random();
+        listBox_unsort.Items.Clear();
+        for (int i = 0; i < n; i++)
+        {
+            X[i] = rnd.Next(0, 100);
+            listBox_unsort.Items.Add(X[i].ToString());
+        }
+
+    }
+    /// <summary>
+    /// Schreibt die sortierten Zahlen in eine List-Box
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void button_aufwaertsSortieren_Click(object sender, EventArgs e)
+    {
+        Sortieren sort = new Sortieren();
+        listBox_sort.Items.Clear();
+        foreach (var x in sort.bubbleSort(X))
+        {
+            listBox_sort.Items.Add(x);
+        }
+    }
+}
+}
