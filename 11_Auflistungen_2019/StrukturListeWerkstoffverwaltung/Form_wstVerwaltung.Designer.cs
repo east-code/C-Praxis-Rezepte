97143bb79fa609613ea4adb84d3e619b01b8bc48
Horst-Christian Heinke <98899060+east-code@users.noreply.github.com>
2022-02-21 13:45:57 +0100

    Add files via upload

---
+++ 11_Auflistungen_2019/StrukturListeWerkstoffverwaltung/Form_wstVerwaltung.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace werkstoffverwaltung
+{
+    partial class Form_matManagement
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_matManagement));
+      this.menuStrip = new System.Windows.Forms.MenuStrip();
+      this.addToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.label_matName = new System.Windows.Forms.Label();
+      this.label_matTension = new System.Windows.Forms.Label();
+      this.label_matDescription = new System.Windows.Forms.Label();
+      this.label_mat = new System.Windows.Forms.Label();
+      this.textBox_matName = new System.Windows.Forms.TextBox();
+      this.textBox_matTension = new System.Windows.Forms.TextBox();
+      this.textBox_matDescription = new System.Windows.Forms.TextBox();
+      this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+      this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+      this.listBox_materials = new System.Windows.Forms.ListBox();
+      this.menuStrip.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // menuStrip
+      // 
+      resources.ApplyResources(this.menuStrip, "menuStrip");
+      this.menuStrip.ImageScalingSize = new System.Drawing.Size(24, 24);
+      this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addToolStripMenuItem,
+            this.deleteToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.loadToolStripMenuItem,
+            this.exitToolStripMenuItem});
+      this.menuStrip.Name = "menuStrip";
+      // 
+      // addToolStripMenuItem
+      // 
+      resources.ApplyResources(this.addToolStripMenuItem, "addToolStripMenuItem");
+      this.addToolStripMenuItem.Name = "addToolStripMenuItem";
+      this.addToolStripMenuItem.Click += new System.EventHandler(this.hinzufügenToolStripMenuItem_Click);
+      // 
+      // deleteToolStripMenuItem
+      // 
+      resources.ApplyResources(this.deleteToolStripMenuItem, "deleteToolStripMenuItem");
+      this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+      this.deleteToolStripMenuItem.Click += new System.EventHandler(this.löschenToolStripMenuItem_Click);
+      // 
+      // saveToolStripMenuItem
+      // 
+      resources.ApplyResources(this.saveToolStripMenuItem, "saveToolStripMenuItem");
+      this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+      this.saveToolStripMenuItem.Click += new System.EventHandler(this.speichernToolStripMenuItem_Click);
+      // 
+      // loadToolStripMenuItem
+      // 
+      resources.ApplyResources(this.loadToolStripMenuItem, "loadToolStripMenuItem");
+      this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+      this.loadToolStripMenuItem.Click += new System.EventHandler(this.ladenToolStripMenuItem_Click);
+      // 
+      // exitToolStripMenuItem
+      // 
+      resources.ApplyResources(this.exitToolStripMenuItem, "exitToolStripMenuItem");
+      this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+      this.exitToolStripMenuItem.Click += new System.EventHandler(this.beendenToolStripMenuItem_Click);
+      // 
+      // label_matName
+      // 
+      resources.ApplyResources(this.label_matName, "label_matName");
+      this.label_matName.ForeColor = System.Drawing.Color.Navy;
+      this.label_matName.Name = "label_matName";
+      // 
+      // label_matTension
+      // 
+      resources.ApplyResources(this.label_matTension, "label_matTension");
+      this.label_matTension.ForeColor = System.Drawing.Color.Navy;
+      this.label_matTension.Name = "label_matTension";
+      // 
+      // label_matDescription
+      // 
+      resources.ApplyResources(this.label_matDescription, "label_matDescription");
+      this.label_matDescription.ForeColor = System.Drawing.Color.Navy;
+      this.label_matDescription.Name = "label_matDescription";
+      // 
+      // label_mat
+      // 
+      resources.ApplyResources(this.label_mat, "label_mat");
+      this.label_mat.Name = "label_mat";
+      // 
+      // textBox_matName
+      // 
+      resources.ApplyResources(this.textBox_matName, "textBox_matName");
+      this.textBox_matName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+      this.textBox_matName.Name = "textBox_matName";
+      // 
+      // textBox_matTension
+      // 
+      resources.ApplyResources(this.textBox_matTension, "textBox_matTension");
+      this.textBox_matTension.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+      this.textBox_matTension.Name = "textBox_matTension";
+      // 
+      // textBox_matDescription
+      // 
+      resources.ApplyResources(this.textBox_matDescription, "textBox_matDescription");
+      this.textBox_matDescription.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+      this.textBox_matDescription.Name = "textBox_matDescription";
+      // 
+      // saveFileDialog
+      // 
+      this.saveFileDialog.DefaultExt = "mat";
+      this.saveFileDialog.FileName = "Materials";
+      resources.ApplyResources(this.saveFileDialog, "saveFileDialog");
+      this.saveFileDialog.InitialDirectory = "C:\\temp";
+      // 
+      // openFileDialog
+      // 
+      this.openFileDialog.DefaultExt = "mat";
+      this.openFileDialog.FileName = "Materials";
+      resources.ApplyResources(this.openFileDialog, "openFileDialog");
+      this.openFileDialog.InitialDirectory = "C:\\temp";
+      // 
+      // listBox_materials
+      // 
+      resources.ApplyResources(this.listBox_materials, "listBox_materials");
+      this.listBox_materials.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+      this.listBox_materials.FormattingEnabled = true;
+      this.listBox_materials.Name = "listBox_materials";
+      this.listBox_materials.SelectedIndexChanged += new System.EventHandler(this.listBox_materials_SelectedIndexChanged);
+      // 
+      // Form_matManagement
+      // 
+      resources.ApplyResources(this, "$this");
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.Controls.Add(this.listBox_materials);
+      this.Controls.Add(this.textBox_matDescription);
+      this.Controls.Add(this.textBox_matTension);
+      this.Controls.Add(this.textBox_matName);
+      this.Controls.Add(this.label_mat);
+      this.Controls.Add(this.label_matDescription);
+      this.Controls.Add(this.label_matTension);
+      this.Controls.Add(this.label_matName);
+      this.Controls.Add(this.menuStrip);
+      this.MainMenuStrip = this.menuStrip;
+      this.Name = "Form_matManagement";
+      this.Load += new System.EventHandler(this.Form_matManagement_Load);
+      this.menuStrip.ResumeLayout(false);
+      this.menuStrip.PerformLayout();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem addToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Label label_matName;
+        private System.Windows.Forms.Label label_matTension;
+        private System.Windows.Forms.Label label_matDescription;
+        private System.Windows.Forms.Label label_mat;
+        private System.Windows.Forms.TextBox textBox_matName;
+        private System.Windows.Forms.TextBox textBox_matTension;
+        private System.Windows.Forms.TextBox textBox_matDescription;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+    private System.Windows.Forms.ListBox listBox_materials;
+  }
+}
+
---
+++ 11_Auflistungen_2019/StrukturListeWerkstoffverwaltung/Form_wstVerwaltung.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace werkstoffverwaltung
+{
+public partial class Form_matManagement : Form
+{
+public Form_matManagement()
+{
+    InitializeComponent();
+}
+#region struct, list, variable
+/// <summary>
+/// Werkstoffmerkmale
+/// </summary>
+private struct MaterialStruct
+{
+  public string name;
+  public double tension;
+  public string descrition;
+}
+
+/// <summary>
+/// Kombination einer Liste mit einer Struktur
+/// </summary>
+private List<MaterialStruct> Materials = new List<MaterialStruct>();
+/// <summary>
+/// Aktuelle Listenzeile (Zeilenindex)
+/// </summary>
+int Line = 0; //aktuelle Zeile innerhalb der Liste
+#endregion struct, list, variables
+
+#region Standardwerte, Anfangswerte
+/// <summary>
+/// Hinzufügen der Standarddatensätze zur Strukturliste
+/// </summary>
+private void Form_matManagement_Load(object sender, EventArgs e)
+{
+
+  // lokale Eingabe
+  MaterialStruct mat = new MaterialStruct();
+
+  // 0. Materialeigenschaften
+  mat.name = "G-X6CrNiMo18-10";
+  mat.tension = 800;
+  mat.descrition = "V4A, Für Pumpen";
+  // Übergabe des Datensatzes an die Liste
+  Materials.Add(mat);
+  // 1.
+  mat.name = "S235JR+AR";
+  mat.tension = 235;
+  mat.descrition = "Re in N/mm², Baustahl";
+  Materials.Add(mat);
+  //2.
+  mat.name = "C45 E+N";
+  mat.tension = 430;
+  mat.descrition = "Rp0.2, Für Zahnräder";
+  Materials.Add(mat);
+  // Ausgabe der internen Lista 
+  foreachOutput();
+  // Setzt den Fokus auf den letzten Datensatz 
+  Line = Materials.Count - 1;
+  // Überträgt den aktuellen Datensatz zu den Editierfeldern
+  showLine();
+}
+#endregion Standardwerte, Anfangswerte
+
+
+#region Elementarmethoden
+/// <summary>
+/// Ausgabe der Listendaten per "foreach loop"
+/// </summary>
+private void foreachOutput()
+{
+  //  Löschen der alten Daten vor erneuter Ausgabe
+  listBox_materials.Items.Clear();
+  //  Ausgabeformat
+  string format ="{0,-18} | {1,7:0.0} | {2,-25}"; 
+  foreach (var mat in Materials)
+  {
+    // Formatierte Ausgabe entsprechend des Formatierungsstrings
+    listBox_materials.Items.Add(String.Format(format, mat.name, mat.tension, mat.descrition));
+  }
+}
+/// <summary>
+/// Shows the data of the current line in the edit fields (Text Boxes).
+/// </summary>
+private void showLine()
+{
+  // Transfer the properties
+  textBox_matName.Text = Materials[Line].name;
+  textBox_matTension.Text = Materials[Line].tension.ToString();
+  textBox_matDescription.Text = Materials[Line].descrition;
+  // Output the recordnumber
+  label_mat.Text = "Werkstoff, Zeile: " + Line.ToString();
+}
+
+
+/// <summary>
+/// Saves all material data in a text file * .mat.
+/// The separator between the properties is "|".
+/// </summary>
+/// <param name="fileName"></param> Path
+private void saveAsTextfile(string fileName)
+{
+  // Deletes, if applicable, existing file
+  System.IO.File.Delete(fileName);
+  // Save in text format (separator: "|")
+  foreach (var mat in Materials)
+    System.IO.File.AppendAllText(fileName, mat.name + " | " +
+                mat.tension + " | " + mat.descrition + "\r\n");
+}
+/// <summary>
+/// Loads the data of the text file and hand over it to the list.
+/// </summary>
+/// <param name="fileName"></param> Path
+private void loadFromTextfile(string fileName)
+{
+  //Removes all elements from the material list
+  Materials.Clear(); 
+  MaterialStruct mat =new MaterialStruct();
+  // Reads all lines of the text file
+  string[] text=System.IO.File.ReadAllLines(fileName);
+  // Characters to be removed
+  char[] charsToTrim = { '\r', '\n', ' ' };
+  // Processes all lines
+  foreach (var line in text){
+    // Separates the string into its elements
+    string[] element = line.Split('|');
+    // Maps the elements to the properties of the material
+    mat.name = element[0].Trim(charsToTrim);
+    mat.tension = Convert.ToDouble(element[1].Trim(charsToTrim));
+    mat.descrition = element[2].Trim(charsToTrim);
+    // Adds the material to the list
+    Materials.Add(mat);
+  }
+}
+    #endregion Elementarmethoden
+
+#region event methods of main menu
+/// <summary>
+/// Adds a record to the list.
+/// </summary>
+private void hinzufügenToolStripMenuItem_Click(object sender, EventArgs e)
+{
+  // Local structure
+  MaterialStruct mat=new MaterialStruct();
+  // Input
+  mat.name = textBox_matName.Text;
+  mat.tension = Convert.ToDouble(textBox_matTension.Text);
+  mat.descrition = textBox_matDescription.Text;
+  // Transfer to the list
+  Materials.Add(mat);
+  // Display
+  foreachOutput();
+}
+
+/// <summary>
+/// Deletes the displayed record 
+/// </summary>
+private void löschenToolStripMenuItem_Click(object sender, EventArgs e)
+{
+  // Local structure
+  MaterialStruct mat = new MaterialStruct();
+  // Data to be deleted
+
+  mat.name = textBox_matName.Text;
+  mat.tension = Convert.ToDouble(textBox_matTension.Text);
+  mat.descrition = textBox_matDescription.Text;
+  // Removes record from the list
+  Materials.Remove(mat);
+  // Update the display
+  foreachOutput();
+}
+/// <summary>
+/// Save the list
+/// </summary>
+private void speichernToolStripMenuItem_Click(object sender, EventArgs e)
+{
+      // Save dialog
+      SaveFileDialog saveFildDialog =new SaveFileDialog();
+      if (saveFileDialog.ShowDialog() == DialogResult.OK) {
+              // Calls the method
+              saveAsTextfile(saveFileDialog.FileName);
+      }
+}
+/// <summary>
+/// Load the list
+/// </summary>
+/// <param name="sender"></param>
+/// <param name="e"></param>
+    private void ladenToolStripMenuItem_Click(object sender, EventArgs e)
+{
+  // Open dialog
+  OpenFileDialog openFileDialog=new OpenFileDialog();
+  if (openFileDialog.ShowDialog() == DialogResult.OK) {
+    // Calls the method
+    loadFromTextfile(openFileDialog.FileName);
+  }
+  // Update the display
+  foreachOutput();
+}
+/// <summary>
+/// Exit the application
+/// </summary>
+private void beendenToolStripMenuItem_Click(object sender, EventArgs e)
+{
+  Close();
+}
+    #endregion event methods of main menu
+
+#region event method of Listbox
+/// <summary>
+/// Gets the index of the active row and record
+/// </summary>
+private void listBox_materials_SelectedIndexChanged(object sender, EventArgs e)
+{
+  // Returns the line text
+  string stringLine = listBox_materials.Text;
+  // Returns the associated index
+  Line = listBox_materials.Items.IndexOf(stringLine);
+  // Check limits
+  if (Line < 0) Line = 0;
+  if (Line > Materials.Count - 1) Line = Materials.Count - 1;
+  // Display elements
+  showLine();
+}
+#endregion event method of Listbox
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  }
+}
