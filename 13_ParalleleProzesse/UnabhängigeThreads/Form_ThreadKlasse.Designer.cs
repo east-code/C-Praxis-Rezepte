bf8b28574a7fa52270fd36f6461c28c75a72e773
Horst-Christian Heinke <98899060+east-code@users.noreply.github.com>
2022-02-21 13:59:24 +0100

    Add files via upload

---
+++ 13_ParalleleProzesse/UnabhängigeThreads/Form_ThreadKlasse.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace ThreadKlasse
+{
+  partial class Form_ThreadKlasse
+  {
+    /// <summary>
+    /// Erforderliche Designervariable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Verwendete Ressourcen bereinigen.
+    /// </summary>
+    /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Vom Windows Form-Designer generierter Code
+
+    /// <summary>
+    /// Erforderliche Methode für die Designerunterstützung.
+    /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.button_Start = new System.Windows.Forms.Button();
+      this.textBox_Feld = new System.Windows.Forms.TextBox();
+      this.textBox_Merkmale = new System.Windows.Forms.TextBox();
+      this.SuspendLayout();
+      // 
+      // button_Start
+      // 
+      this.button_Start.Location = new System.Drawing.Point(67, 116);
+      this.button_Start.Margin = new System.Windows.Forms.Padding(5);
+      this.button_Start.Name = "button_Start";
+      this.button_Start.Size = new System.Drawing.Size(296, 82);
+      this.button_Start.TabIndex = 1;
+      this.button_Start.Text = "Start der Threads";
+      this.button_Start.UseVisualStyleBackColor = true;
+      this.button_Start.Click += new System.EventHandler(this.button_Start_Click);
+      // 
+      // textBox_Feld
+      // 
+      this.textBox_Feld.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+      this.textBox_Feld.Dock = System.Windows.Forms.DockStyle.Top;
+      this.textBox_Feld.Location = new System.Drawing.Point(0, 0);
+      this.textBox_Feld.Multiline = true;
+      this.textBox_Feld.Name = "textBox_Feld";
+      this.textBox_Feld.Size = new System.Drawing.Size(471, 88);
+      this.textBox_Feld.TabIndex = 2;
+      // 
+      // textBox_Merkmale
+      // 
+      this.textBox_Merkmale.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+      this.textBox_Merkmale.Dock = System.Windows.Forms.DockStyle.Bottom;
+      this.textBox_Merkmale.Location = new System.Drawing.Point(0, 229);
+      this.textBox_Merkmale.Multiline = true;
+      this.textBox_Merkmale.Name = "textBox_Merkmale";
+      this.textBox_Merkmale.Size = new System.Drawing.Size(471, 137);
+      this.textBox_Merkmale.TabIndex = 3;
+      // 
+      // Form_ThreadKlasse
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 32F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(471, 366);
+      this.Controls.Add(this.textBox_Merkmale);
+      this.Controls.Add(this.textBox_Feld);
+      this.Controls.Add(this.button_Start);
+      this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.Margin = new System.Windows.Forms.Padding(5);
+      this.Name = "Form_ThreadKlasse";
+      this.Text = "Threads";
+      this.Load += new System.EventHandler(this.Form_ThreadKlasse_Load);
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button button_Start;
+    private System.Windows.Forms.TextBox textBox_Feld;
+    private System.Windows.Forms.TextBox textBox_Merkmale;
+  }
+}
+
---
+++ 13_ParalleleProzesse/UnabhängigeThreads/Form_ThreadKlasse.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;      // wird für Threads benötigt
+
+namespace ThreadKlasse
+{
+  public partial class Form_ThreadKlasse : Form
+  {
+    public Form_ThreadKlasse()
+    {
+      InitializeComponent();
+    }
+
+
+/// <summary>
+/// Feld mit Testdaten
+/// </summary>
+    Int32[] feld = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 9, 7, 5, 3, 1};
+
+/// <summary>
+/// Ausgabe der Daten des Feldes zur Kontrolle
+/// </summary>
+private void Form_ThreadKlasse_Load(object sender, EventArgs e)
+{
+  textBox_Feld.Text += "Daten des Feldes\r\n";
+  for (int i = 0; i < feld.Length; i++) textBox_Feld.Text += feld[i] + ", ";
+}
+
+/// <summary>
+/// Einsatz der Threads zur Berechnung
+/// des Mittelwertes und zur Ermittlung des Maximums
+/// </summary>
+private void button_Start_Click(object sender, EventArgs e)
+{
+  // Instanz der Klasse
+  Klasse.Work work = new Klasse.Work();
+  // Übergabe der Daten per Eigenschaftsmethoden
+  work.setFeld = feld;
+  // Threadinstanzen mit Funktionszeiger als Parameter
+  Thread threadMaximum = new Thread(work.Maximum);
+  Thread threadMittelwert = new Thread(work.Mittelwert);
+  // Start der beiden Threads
+  threadMaximum.Start();
+  threadMittelwert.Start();
+  // Die Ausgabe kann erst dann sinnvolle Ergebnisse liefern, nachdem
+  // beide Threads beendet wurden. Die Methode Join() sorgt dafür,
+  // das der Hauptprozess erst nach Ende der Threads
+  // threadMittelwert und threadMaximum weiterläuft. 
+  threadMittelwert.Join();
+  threadMaximum.Join();
+  // Ausgabe der Ergebnisse mit Hilfe Eigenschaftsmethoden
+  textBox_Merkmale.Text += "Maximum   : " + work.getMaximum +"\r\n";
+  textBox_Merkmale.Text += "Mittelwert: " + work.getMittelwert + "\r\n"; 
+}
+
+
+  }
+}
+
+
+// Daher eine Pause (0.1 s) im Hauptprozess mit: Thread.Sleep(100);
