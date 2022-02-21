fbd9dc902924e1aa889ed87ec07e1b1fb1fc2ca7
Horst-Christian Heinke <98899060+east-code@users.noreply.github.com>
2022-02-21 13:32:51 +0100

    Add files via upload

---
+++ 09-3_Felder_Vektoraddition/Vektoraddition/Form_Vektoraddition.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace Vektoraddition
+{
+    partial class Form_Vektoraddition
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+      this.textBox_V1x = new System.Windows.Forms.TextBox();
+      this.textBox_V1y = new System.Windows.Forms.TextBox();
+      this.textBox_V1z = new System.Windows.Forms.TextBox();
+      this.textBox_V2x = new System.Windows.Forms.TextBox();
+      this.textBox_V2y = new System.Windows.Forms.TextBox();
+      this.textBox_V2z = new System.Windows.Forms.TextBox();
+      this.textBox_VRz = new System.Windows.Forms.TextBox();
+      this.textBox_VRy = new System.Windows.Forms.TextBox();
+      this.textBox_VRx = new System.Windows.Forms.TextBox();
+      this.label_Vector1 = new System.Windows.Forms.Label();
+      this.label1 = new System.Windows.Forms.Label();
+      this.label2 = new System.Windows.Forms.Label();
+      this.label3 = new System.Windows.Forms.Label();
+      this.label4 = new System.Windows.Forms.Label();
+      this.label5 = new System.Windows.Forms.Label();
+      this.button_Addition = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // textBox_V1x
+      // 
+      this.textBox_V1x.BackColor = System.Drawing.SystemColors.ActiveCaption;
+      this.textBox_V1x.Location = new System.Drawing.Point(226, 55);
+      this.textBox_V1x.Margin = new System.Windows.Forms.Padding(8, 9, 8, 9);
+      this.textBox_V1x.Name = "textBox_V1x";
+      this.textBox_V1x.Size = new System.Drawing.Size(97, 44);
+      this.textBox_V1x.TabIndex = 0;
+      // 
+      // textBox_V1y
+      // 
+      this.textBox_V1y.BackColor = System.Drawing.SystemColors.ActiveCaption;
+      this.textBox_V1y.Location = new System.Drawing.Point(339, 56);
+      this.textBox_V1y.Margin = new System.Windows.Forms.Padding(8, 9, 8, 9);
+      this.textBox_V1y.Name = "textBox_V1y";
+      this.textBox_V1y.Size = new System.Drawing.Size(84, 44);
+      this.textBox_V1y.TabIndex = 1;
+      // 
+      // textBox_V1z
+      // 
+      this.textBox_V1z.BackColor = System.Drawing.SystemColors.ActiveCaption;
+      this.textBox_V1z.Location = new System.Drawing.Point(439, 55);
+      this.textBox_V1z.Margin = new System.Windows.Forms.Padding(8, 9, 8, 9);
+      this.textBox_V1z.Name = "textBox_V1z";
+      this.textBox_V1z.Size = new System.Drawing.Size(84, 44);
+      this.textBox_V1z.TabIndex = 2;
+      // 
+      // textBox_V2x
+      // 
+      this.textBox_V2x.BackColor = System.Drawing.SystemColors.ActiveCaption;
+      this.textBox_V2x.Location = new System.Drawing.Point(226, 118);
+      this.textBox_V2x.Margin = new System.Windows.Forms.Padding(8, 9, 8, 9);
+      this.textBox_V2x.Name = "textBox_V2x";
+      this.textBox_V2x.Size = new System.Drawing.Size(85, 44);
+      this.textBox_V2x.TabIndex = 3;
+      // 
+      // textBox_V2y
+      // 
+      this.textBox_V2y.BackColor = System.Drawing.SystemColors.ActiveCaption;
+      this.textBox_V2y.Location = new System.Drawing.Point(339, 118);
+      this.textBox_V2y.Margin = new System.Windows.Forms.Padding(8, 9, 8, 9);
+      this.textBox_V2y.Name = "textBox_V2y";
+      this.textBox_V2y.Size = new System.Drawing.Size(84, 44);
+      this.textBox_V2y.TabIndex = 4;
+      // 
+      // textBox_V2z
+      // 
+      this.textBox_V2z.BackColor = System.Drawing.SystemColors.ActiveCaption;
+      this.textBox_V2z.Location = new System.Drawing.Point(439, 118);
+      this.textBox_V2z.Margin = new System.Windows.Forms.Padding(8, 9, 8, 9);
+      this.textBox_V2z.Name = "textBox_V2z";
+      this.textBox_V2z.Size = new System.Drawing.Size(84, 44);
+      this.textBox_V2z.TabIndex = 5;
+      // 
+      // textBox_VRz
+      // 
+      this.textBox_VRz.BackColor = System.Drawing.SystemColors.ControlLight;
+      this.textBox_VRz.Location = new System.Drawing.Point(439, 258);
+      this.textBox_VRz.Margin = new System.Windows.Forms.Padding(8, 9, 8, 9);
+      this.textBox_VRz.Name = "textBox_VRz";
+      this.textBox_VRz.Size = new System.Drawing.Size(85, 44);
+      this.textBox_VRz.TabIndex = 8;
+      // 
+      // textBox_VRy
+      // 
+      this.textBox_VRy.BackColor = System.Drawing.SystemColors.ControlLight;
+      this.textBox_VRy.Location = new System.Drawing.Point(338, 258);
+      this.textBox_VRy.Margin = new System.Windows.Forms.Padding(8, 9, 8, 9);
+      this.textBox_VRy.Name = "textBox_VRy";
+      this.textBox_VRy.Size = new System.Drawing.Size(85, 44);
+      this.textBox_VRy.TabIndex = 7;
+      // 
+      // textBox_VRx
+      // 
+      this.textBox_VRx.BackColor = System.Drawing.SystemColors.ControlLight;
+      this.textBox_VRx.Location = new System.Drawing.Point(240, 258);
+      this.textBox_VRx.Margin = new System.Windows.Forms.Padding(8, 9, 8, 9);
+      this.textBox_VRx.Name = "textBox_VRx";
+      this.textBox_VRx.Size = new System.Drawing.Size(85, 44);
+      this.textBox_VRx.TabIndex = 6;
+      // 
+      // label_Vector1
+      // 
+      this.label_Vector1.AutoSize = true;
+      this.label_Vector1.Location = new System.Drawing.Point(15, 58);
+      this.label_Vector1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+      this.label_Vector1.Name = "label_Vector1";
+      this.label_Vector1.Size = new System.Drawing.Size(135, 37);
+      this.label_Vector1.TabIndex = 9;
+      this.label_Vector1.Text = "Vektor 1";
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(15, 118);
+      this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(137, 37);
+      this.label1.TabIndex = 10;
+      this.label1.Text = "Vektor 2";
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(6, 261);
+      this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(229, 37);
+      this.label2.TabIndex = 11;
+      this.label2.Text = "Ergebnisvektor";
+      // 
+      // label3
+      // 
+      this.label3.AutoSize = true;
+      this.label3.Location = new System.Drawing.Point(260, 9);
+      this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(32, 37);
+      this.label3.TabIndex = 9;
+      this.label3.Text = "x";
+      // 
+      // label4
+      // 
+      this.label4.AutoSize = true;
+      this.label4.Location = new System.Drawing.Point(358, 9);
+      this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+      this.label4.Name = "label4";
+      this.label4.Size = new System.Drawing.Size(32, 37);
+      this.label4.TabIndex = 12;
+      this.label4.Text = "y";
+      // 
+      // label5
+      // 
+      this.label5.AutoSize = true;
+      this.label5.Location = new System.Drawing.Point(463, 9);
+      this.label5.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+      this.label5.Name = "label5";
+      this.label5.Size = new System.Drawing.Size(33, 37);
+      this.label5.TabIndex = 13;
+      this.label5.Text = "z";
+      // 
+      // button_Addition
+      // 
+      this.button_Addition.Location = new System.Drawing.Point(22, 177);
+      this.button_Addition.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+      this.button_Addition.Name = "button_Addition";
+      this.button_Addition.Size = new System.Drawing.Size(502, 61);
+      this.button_Addition.TabIndex = 15;
+      this.button_Addition.Text = "Addition der Vektoren +";
+      this.button_Addition.UseVisualStyleBackColor = true;
+      this.button_Addition.Click += new System.EventHandler(this.button_Addition_Click);
+      // 
+      // Form_Vektoraddition
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 37F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(550, 324);
+      this.Controls.Add(this.button_Addition);
+      this.Controls.Add(this.label5);
+      this.Controls.Add(this.label4);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.label3);
+      this.Controls.Add(this.label_Vector1);
+      this.Controls.Add(this.textBox_VRz);
+      this.Controls.Add(this.textBox_VRy);
+      this.Controls.Add(this.textBox_VRx);
+      this.Controls.Add(this.textBox_V2z);
+      this.Controls.Add(this.textBox_V2y);
+      this.Controls.Add(this.textBox_V2x);
+      this.Controls.Add(this.textBox_V1z);
+      this.Controls.Add(this.textBox_V1y);
+      this.Controls.Add(this.textBox_V1x);
+      this.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+      this.Margin = new System.Windows.Forms.Padding(8, 9, 8, 9);
+      this.Name = "Form_Vektoraddition";
+      this.Text = "Vektoraddition";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_V1x;
+        private System.Windows.Forms.TextBox textBox_V1y;
+        private System.Windows.Forms.TextBox textBox_V1z;
+        private System.Windows.Forms.TextBox textBox_V2x;
+        private System.Windows.Forms.TextBox textBox_V2y;
+        private System.Windows.Forms.TextBox textBox_V2z;
+        private System.Windows.Forms.TextBox textBox_VRz;
+        private System.Windows.Forms.TextBox textBox_VRy;
+        private System.Windows.Forms.TextBox textBox_VRx;
+        private System.Windows.Forms.Label label_Vector1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button_Addition;
+    }
+}
+
---
+++ 09-3_Felder_Vektoraddition/Vektoraddition/Form_Vektoraddition.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vektoraddition
+{
+    public partial class Form_Vektoraddition : Form
+    {
+        public Form_Vektoraddition()
+        {
+            InitializeComponent();
+        }
+// <summary>
+/// Addition zweier Vektoren
+/// </summary>
+/// <param name="sender"></param>
+/// <param name="e"></param>
+private void button_Addition_Click(object sender, EventArgs e)
+{
+  // Drei Vektoren, von denen jeder drei Elemente besitzt.
+  // Der Datentyp der Elemente ist reell (double).
+  double[] Vektor1 = new double[3];
+  double[] Vektor2 = new double[3];
+  double[] VektorResult = new double[3];
+
+  // Stringfelder für die Eingabekomponenten
+  // sind möglich. Dadurch lassen sich Schleifen
+  // während der Eingabe verwenden. Die Größe
+  // der Felder und der Inhalt der Feldelemente
+  // ergibt sich aus der in geschweiften Klammern
+  // stehenden Werteliste.
+  string[] textBox1 = { textBox_V1x.Text, textBox_V1y.Text, textBox_V1z.Text };
+  string[] textBox2 = { textBox_V2x.Text, textBox_V2y.Text, textBox_V2z.Text };
+ 
+  // Daten aus dem Formular
+  for (int i=0; i<3; i++)
+  {
+    Vektor1[i] = Convert.ToDouble(textBox1[i]);
+    Vektor2[i] = Convert.ToDouble(textBox2[i]);
+  }
+
+  // Addition der Felder
+  for (int i=0; i<3; i++)
+  {
+    VektorResult[i] = Vektor1[i] + Vektor2[i];
+  }
+
+  //Ausgabe
+  textBox_VRx.Text = VektorResult[0].ToString();
+  textBox_VRy.Text = VektorResult[1].ToString();
+  textBox_VRz.Text = VektorResult[2].ToString();
+}                                 
+    }
+}
