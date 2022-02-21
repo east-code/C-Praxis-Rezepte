61c7cbcd8c6cef37af9c9eaf08c344b40abc7f28
Horst-Christian Heinke <98899060+east-code@users.noreply.github.com>
2022-02-21 13:25:34 +0100

    Add files via upload

---
+++ gdiMomentenverlauf/Form_Momentenverlauf.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Momentenverlauf
+{
+    partial class Form_Momentenverlauf
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_Momentenverlauf));
+            this.button_OK = new System.Windows.Forms.Button();
+            this.label_l = new System.Windows.Forms.Label();
+            this.label_q = new System.Windows.Forms.Label();
+            this.textBox_l = new System.Windows.Forms.TextBox();
+            this.textBox_q = new System.Windows.Forms.TextBox();
+            this.panel_BildTraeger = new System.Windows.Forms.Panel();
+            this.panel_diagramm = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // button_OK
+            // 
+            this.button_OK.Image = ((System.Drawing.Image)(resources.GetObject("button_OK.Image")));
+            this.button_OK.Location = new System.Drawing.Point(471, 306);
+            this.button_OK.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.button_OK.Name = "button_OK";
+            this.button_OK.Size = new System.Drawing.Size(248, 74);
+            this.button_OK.TabIndex = 0;
+            this.button_OK.Text = "OK";
+            this.button_OK.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button_OK.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button_OK.UseVisualStyleBackColor = true;
+            this.button_OK.Click += new System.EventHandler(this.button_OK_Click);
+            // 
+            // label_l
+            // 
+            this.label_l.AutoSize = true;
+            this.label_l.Location = new System.Drawing.Point(9, 312);
+            this.label_l.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label_l.Name = "label_l";
+            this.label_l.Size = new System.Drawing.Size(250, 25);
+            this.label_l.TabIndex = 1;
+            this.label_l.Text = "Länge des Trägers l in m";
+            // 
+            // label_q
+            // 
+            this.label_q.AutoSize = true;
+            this.label_q.Location = new System.Drawing.Point(9, 355);
+            this.label_q.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label_q.Name = "label_q";
+            this.label_q.Size = new System.Drawing.Size(241, 25);
+            this.label_q.TabIndex = 2;
+            this.label_q.Text = "Streckenlast q(z) in N/m";
+            // 
+            // textBox_l
+            // 
+            this.textBox_l.BackColor = System.Drawing.Color.LightBlue;
+            this.textBox_l.Location = new System.Drawing.Point(269, 306);
+            this.textBox_l.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.textBox_l.Name = "textBox_l";
+            this.textBox_l.Size = new System.Drawing.Size(180, 31);
+            this.textBox_l.TabIndex = 3;
+            this.textBox_l.Text = "2";
+            // 
+            // textBox_q
+            // 
+            this.textBox_q.BackColor = System.Drawing.Color.LightBlue;
+            this.textBox_q.Location = new System.Drawing.Point(269, 349);
+            this.textBox_q.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.textBox_q.Name = "textBox_q";
+            this.textBox_q.Size = new System.Drawing.Size(180, 31);
+            this.textBox_q.TabIndex = 4;
+            this.textBox_q.Text = "100";
+            // 
+            // panel_BildTraeger
+            // 
+            this.panel_BildTraeger.BackColor = System.Drawing.SystemColors.Window;
+            this.panel_BildTraeger.BackgroundImage = global::Momentenverlauf.Properties.Resources.EingespannterTrägerOnly200dpi;
+            this.panel_BildTraeger.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.panel_BildTraeger.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel_BildTraeger.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel_BildTraeger.Location = new System.Drawing.Point(0, 0);
+            this.panel_BildTraeger.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.panel_BildTraeger.Name = "panel_BildTraeger";
+            this.panel_BildTraeger.Size = new System.Drawing.Size(733, 284);
+            this.panel_BildTraeger.TabIndex = 6;
+            // 
+            // panel_diagramm
+            // 
+            this.panel_diagramm.BackColor = System.Drawing.Color.Gainsboro;
+            this.panel_diagramm.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel_diagramm.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel_diagramm.Location = new System.Drawing.Point(0, 394);
+            this.panel_diagramm.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.panel_diagramm.Name = "panel_diagramm";
+            this.panel_diagramm.Size = new System.Drawing.Size(733, 370);
+            this.panel_diagramm.TabIndex = 7;
+            this.panel_diagramm.Paint += new System.Windows.Forms.PaintEventHandler(this.panel_Dag_Paint);
+            this.panel_diagramm.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel_Dag_MouseClick);
+            // 
+            // Form_Momentenverlauf
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(733, 764);
+            this.Controls.Add(this.panel_diagramm);
+            this.Controls.Add(this.panel_BildTraeger);
+            this.Controls.Add(this.textBox_q);
+            this.Controls.Add(this.textBox_l);
+            this.Controls.Add(this.label_q);
+            this.Controls.Add(this.label_l);
+            this.Controls.Add(this.button_OK);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "Form_Momentenverlauf";
+            this.Text = "Momentenverlauf: Eingespannter Träger";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_OK;
+        private System.Windows.Forms.Label label_l;
+        private System.Windows.Forms.Label label_q;
+        private System.Windows.Forms.TextBox textBox_l;
+        private System.Windows.Forms.TextBox textBox_q;
+        private System.Windows.Forms.Panel panel_BildTraeger;
+        private System.Windows.Forms.Panel panel_diagramm;
+    }
+}
+
---
+++ gdiMomentenverlauf/Form_Momentenverlauf.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Momentenverlauf
+{
+    public partial class Form_Momentenverlauf : Form
+    {
+        public Form_Momentenverlauf()
+        {
+            InitializeComponent();
+        }
+
+private void button_OK_Click(object sender, EventArgs e)
+{
+    const int N = 101;  //Anzahl der Stützpunkte als Konstante
+    double l = Convert.ToDouble(textBox_l.Text); //Eingabe
+    double q = Convert.ToDouble(textBox_q.Text);
+    double dz =  l / (N - 1);  //Schrittweite in z
+    Graphics diagramm = panel_diagramm.CreateGraphics(); //panel als Zeichenfläche
+    diagramm.Clear(panel_diagramm.BackColor);             //Löschen des Panels      
+    Pen stift=new Pen(Color.Blue,2);//Stift rot, 2 Pixel breit
+    int rand = 50;      //Rand zur Diagrammfläche
+    int abstand = 10;   //Abstand zu einer Kante oder Linie
+    Rectangle flaeche = new Rectangle(rand, rand, panel_diagramm.Width - 2*rand, panel_diagramm.Height - 2*rand);
+    diagramm.DrawRectangle(stift, flaeche); //Umrandung zur Diagrammfläche
+    Font font = new Font("Microsoft Sans Serif", 16); //Schriftart //Microsoft Sans Serif; 15,75pt
+    SolidBrush brush = new SolidBrush(Color.Blue); //Schriftfarbe
+    diagramm.DrawString("<-- Koordinate z", font, brush, rand + 150, abstand); //Ausgabe
+    StringFormat richtung = new StringFormat(StringFormatFlags.DirectionVertical);
+    //diagramm.RotateTransform(270);
+    diagramm.DrawString("Biegemoment Mb(z) -->", font, brush, flaeche.Width + rand + abstand, rand + abstand, richtung);
+    //Anfangspunkt P0
+    float z0  = (float) (dz*0);  //z-Koordinate berechnet aus Schrittweite und Index
+    float Mb0 = (float) (q * z0 * z0 / 2);  //Biegemoment an der Stelle z
+    Pen stiftFunktion=new Pen(Color.DarkRed,3);
+    float massstabZ = (float) (flaeche.Width / l); //Massstab zum Zeichnen
+    float MbMax = (float)(q * l * l / 2); 
+    float massstabMb = (float)(flaeche.Height / MbMax);
+    for (int i = 1; i < N; i++)
+    {
+        float z  = (float) dz*i;  //z-Koordinate berechnet aus Schrittweite und Index
+        float Mb = (float) (q * z * z / 2);  //Biegemoment an der Stelle z
+        //Zeichnen der Funktion, Hinweis: DrawLine erlaubt keinen Datentyp "double" 
+        diagramm.DrawLine(stiftFunktion, rand+flaeche.Width - z0*massstabZ, rand+Mb0 * massstabMb, rand+flaeche.Width - z*massstabZ, rand+Mb*massstabMb);
+        z0 = z;
+        Mb0 = Mb;
+    }
+    brush = new SolidBrush(Color.DarkRed); //Schriftfarbe
+    diagramm.DrawString(MbMax.ToString("0.00") + " Nm", font, brush, abstand, flaeche.Height + rand + 2);
+}
+       
+        private void chart_Mb_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel_Dag_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel_Dag_MouseClick(object sender, MouseEventArgs e)
+        {
+
+        }
+    }
+}
