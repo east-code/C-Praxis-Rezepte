20297cc1f40b90b54489fc2bf96e069c2f05d179
Horst-Christian Heinke <98899060+east-code@users.noreply.github.com>
2022-02-21 13:21:00 +0100

    Add files via upload

---
+++ 08_DiagrammGrafik/chartEpizykloide/Form_Epizykloide.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace chartEpizykloide
+{
+  partial class Form_Epizykloide
+  {
+    /// <summary>
+    /// Erforderliche Designervariable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Verwendete Ressourcen bereinigen.
+    /// </summary>
+    /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Vom Windows Form-Designer generierter Code
+
+    /// <summary>
+    /// Erforderliche Methode für die Designerunterstützung.
+    /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+      System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+      System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+      System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+      System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+      System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+      this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+      this.epizykloideToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.beendenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.splitContainer_epizykloide = new System.Windows.Forms.SplitContainer();
+      this.label2 = new System.Windows.Forms.Label();
+      this.label1 = new System.Windows.Forms.Label();
+      this.label_r_ = new System.Windows.Forms.Label();
+      this.label_R = new System.Windows.Forms.Label();
+      this.textBox_r_ = new System.Windows.Forms.TextBox();
+      this.textBox_R = new System.Windows.Forms.TextBox();
+      this.chart_epizykloide = new System.Windows.Forms.DataVisualization.Charting.Chart();
+      this.menuStrip1.SuspendLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.splitContainer_epizykloide)).BeginInit();
+      this.splitContainer_epizykloide.Panel1.SuspendLayout();
+      this.splitContainer_epizykloide.Panel2.SuspendLayout();
+      this.splitContainer_epizykloide.SuspendLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.chart_epizykloide)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // menuStrip1
+      // 
+      this.menuStrip1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+      this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.epizykloideToolStripMenuItem,
+            this.beendenToolStripMenuItem});
+      this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+      this.menuStrip1.Name = "menuStrip1";
+      this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 1, 0, 1);
+      this.menuStrip1.Size = new System.Drawing.Size(595, 27);
+      this.menuStrip1.TabIndex = 0;
+      this.menuStrip1.Text = "menuStrip1";
+      // 
+      // epizykloideToolStripMenuItem
+      // 
+      this.epizykloideToolStripMenuItem.Name = "epizykloideToolStripMenuItem";
+      this.epizykloideToolStripMenuItem.Size = new System.Drawing.Size(100, 25);
+      this.epizykloideToolStripMenuItem.Text = "Epizykloide";
+      this.epizykloideToolStripMenuItem.Click += new System.EventHandler(this.epizykloideToolStripMenuItem_Click);
+      // 
+      // beendenToolStripMenuItem
+      // 
+      this.beendenToolStripMenuItem.Name = "beendenToolStripMenuItem";
+      this.beendenToolStripMenuItem.Size = new System.Drawing.Size(82, 25);
+      this.beendenToolStripMenuItem.Text = "Beenden";
+      this.beendenToolStripMenuItem.Click += new System.EventHandler(this.beendenToolStripMenuItem_Click);
+      // 
+      // splitContainer_epizykloide
+      // 
+      this.splitContainer_epizykloide.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.splitContainer_epizykloide.Location = new System.Drawing.Point(0, 27);
+      this.splitContainer_epizykloide.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+      this.splitContainer_epizykloide.Name = "splitContainer_epizykloide";
+      // 
+      // splitContainer_epizykloide.Panel1
+      // 
+      this.splitContainer_epizykloide.Panel1.AutoScroll = true;
+      this.splitContainer_epizykloide.Panel1.AutoScrollMinSize = new System.Drawing.Size(300, 0);
+      this.splitContainer_epizykloide.Panel1.Controls.Add(this.label2);
+      this.splitContainer_epizykloide.Panel1.Controls.Add(this.label1);
+      this.splitContainer_epizykloide.Panel1.Controls.Add(this.label_r_);
+      this.splitContainer_epizykloide.Panel1.Controls.Add(this.label_R);
+      this.splitContainer_epizykloide.Panel1.Controls.Add(this.textBox_r_);
+      this.splitContainer_epizykloide.Panel1.Controls.Add(this.textBox_R);
+      this.splitContainer_epizykloide.Panel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.splitContainer_epizykloide.Panel1.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+      // 
+      // splitContainer_epizykloide.Panel2
+      // 
+      this.splitContainer_epizykloide.Panel2.Controls.Add(this.chart_epizykloide);
+      this.splitContainer_epizykloide.Size = new System.Drawing.Size(595, 810);
+      this.splitContainer_epizykloide.SplitterDistance = 146;
+      this.splitContainer_epizykloide.SplitterWidth = 3;
+      this.splitContainer_epizykloide.TabIndex = 1;
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(8, 60);
+      this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(64, 18);
+      this.label2.TabIndex = 5;
+      this.label2.Text = "R in mm";
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(8, 190);
+      this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(58, 18);
+      this.label1.TabIndex = 4;
+      this.label1.Text = "r in mm";
+      this.label1.Click += new System.EventHandler(this.label_r__Click);
+      // 
+      // label_r_
+      // 
+      this.label_r_.AutoSize = true;
+      this.label_r_.Location = new System.Drawing.Point(8, 173);
+      this.label_r_.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+      this.label_r_.Name = "label_r_";
+      this.label_r_.Size = new System.Drawing.Size(117, 18);
+      this.label_r_.TabIndex = 4;
+      this.label_r_.Text = "Abrollkreisradius";
+      this.label_r_.Click += new System.EventHandler(this.label_r__Click);
+      // 
+      // label_R
+      // 
+      this.label_R.AutoSize = true;
+      this.label_R.Location = new System.Drawing.Point(8, 43);
+      this.label_R.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+      this.label_R.Name = "label_R";
+      this.label_R.Size = new System.Drawing.Size(142, 18);
+      this.label_R.TabIndex = 3;
+      this.label_R.Text = "Führungskreisradius";
+      // 
+      // textBox_r_
+      // 
+      this.textBox_r_.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+      this.textBox_r_.Location = new System.Drawing.Point(7, 214);
+      this.textBox_r_.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+      this.textBox_r_.Name = "textBox_r_";
+      this.textBox_r_.Size = new System.Drawing.Size(71, 24);
+      this.textBox_r_.TabIndex = 1;
+      this.textBox_r_.Text = "20";
+      this.textBox_r_.TextChanged += new System.EventHandler(this.textBox_r__TextChanged);
+      // 
+      // textBox_R
+      // 
+      this.textBox_R.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+      this.textBox_R.Location = new System.Drawing.Point(8, 79);
+      this.textBox_R.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+      this.textBox_R.Name = "textBox_R";
+      this.textBox_R.Size = new System.Drawing.Size(73, 24);
+      this.textBox_R.TabIndex = 0;
+      this.textBox_R.Tag = "";
+      this.textBox_R.Text = "20";
+      // 
+      // chart_epizykloide
+      // 
+      chartArea1.AlignWithChartArea = "ChartArea_Y";
+      chartArea1.AxisX.IntervalAutoMode = System.Windows.Forms.DataVisualization.Charting.IntervalAutoMode.VariableCount;
+      chartArea1.AxisX.IsMarginVisible = false;
+      chartArea1.AxisX.LabelAutoFitMaxFontSize = 11;
+      chartArea1.AxisX.LabelAutoFitMinFontSize = 8;
+      chartArea1.AxisX.LabelStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      chartArea1.AxisX.MajorGrid.Interval = 0D;
+      chartArea1.AxisX.MinorGrid.LineColor = System.Drawing.Color.DarkGray;
+      chartArea1.AxisX.MinorGrid.LineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dot;
+      chartArea1.AxisX.MinorTickMark.LineWidth = 2;
+      chartArea1.AxisX.Title = "x in mm";
+      chartArea1.AxisX.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      chartArea1.AxisY.IntervalAutoMode = System.Windows.Forms.DataVisualization.Charting.IntervalAutoMode.VariableCount;
+      chartArea1.AxisY.IsLabelAutoFit = false;
+      chartArea1.AxisY.LabelAutoFitMaxFontSize = 11;
+      chartArea1.AxisY.LabelAutoFitMinFontSize = 8;
+      chartArea1.AxisY.LabelStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      chartArea1.AxisY.MajorGrid.Interval = 0D;
+      chartArea1.AxisY.MinorGrid.LineColor = System.Drawing.Color.DarkGray;
+      chartArea1.AxisY.MinorGrid.LineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dot;
+      chartArea1.AxisY.MinorTickMark.LineWidth = 2;
+      chartArea1.AxisY.Title = "y in mm";
+      chartArea1.AxisY.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      chartArea1.Name = "ChartArea_Epizykloide";
+      chartArea1.Position.Auto = false;
+      chartArea1.Position.Height = 70F;
+      chartArea1.Position.Width = 94F;
+      chartArea1.Position.X = 3F;
+      chartArea1.Position.Y = 5F;
+      chartArea2.AlignWithChartArea = "ChartArea_Epizykloide";
+      chartArea2.AxisX.IntervalAutoMode = System.Windows.Forms.DataVisualization.Charting.IntervalAutoMode.VariableCount;
+      chartArea2.AxisX.IsLabelAutoFit = false;
+      chartArea2.AxisX.IsMarginVisible = false;
+      chartArea2.AxisX.LabelAutoFitMaxFontSize = 11;
+      chartArea2.AxisX.LabelStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      chartArea2.AxisX.LabelStyle.Interval = 0D;
+      chartArea2.AxisX.MajorGrid.Interval = 0D;
+      chartArea2.AxisX.MajorGrid.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Auto;
+      chartArea2.AxisX.MajorTickMark.Interval = 0D;
+      chartArea2.AxisX.Maximum = 180D;
+      chartArea2.AxisX.MaximumAutoSize = 10F;
+      chartArea2.AxisX.Minimum = -180D;
+      chartArea2.AxisX.ScaleBreakStyle.CollapsibleSpaceThreshold = 20;
+      chartArea2.AxisX.ScaleBreakStyle.Enabled = true;
+      chartArea2.AxisX.ScaleBreakStyle.Spacing = 2D;
+      chartArea2.AxisX.Title = "alpha in °";
+      chartArea2.AxisX.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      chartArea2.AxisY.IntervalAutoMode = System.Windows.Forms.DataVisualization.Charting.IntervalAutoMode.VariableCount;
+      chartArea2.AxisY.IsLabelAutoFit = false;
+      chartArea2.AxisY.LabelAutoFitMaxFontSize = 11;
+      chartArea2.AxisY.LabelStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      chartArea2.AxisY.LabelStyle.Interval = 0D;
+      chartArea2.AxisY.Title = "y in mm, y\' in mm/°";
+      chartArea2.AxisY.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      chartArea2.Name = "ChartArea_Y";
+      chartArea2.Position.Auto = false;
+      chartArea2.Position.Height = 25F;
+      chartArea2.Position.Width = 94F;
+      chartArea2.Position.X = 3F;
+      chartArea2.Position.Y = 75F;
+      this.chart_epizykloide.ChartAreas.Add(chartArea1);
+      this.chart_epizykloide.ChartAreas.Add(chartArea2);
+      this.chart_epizykloide.Dock = System.Windows.Forms.DockStyle.Fill;
+      legend1.Docking = System.Windows.Forms.DataVisualization.Charting.Docking.Top;
+      legend1.LegendStyle = System.Windows.Forms.DataVisualization.Charting.LegendStyle.Row;
+      legend1.Name = "Legend1";
+      legend1.Position.Auto = false;
+      legend1.Position.Height = 5F;
+      legend1.Position.Width = 87F;
+      this.chart_epizykloide.Legends.Add(legend1);
+      this.chart_epizykloide.Location = new System.Drawing.Point(0, 0);
+      this.chart_epizykloide.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+      this.chart_epizykloide.Name = "chart_epizykloide";
+      series1.BorderWidth = 2;
+      series1.ChartArea = "ChartArea_Epizykloide";
+      series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+      series1.Color = System.Drawing.Color.Green;
+      series1.Legend = "Legend1";
+      series1.LegendText = "Epizykloide";
+      series1.Name = "Series_Epizykloide";
+      series2.BorderWidth = 2;
+      series2.ChartArea = "ChartArea_Y";
+      series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+      series2.Color = System.Drawing.Color.Green;
+      series2.Legend = "Legend1";
+      series2.LegendText = "y-Koordinate";
+      series2.Name = "Series_y";
+      series3.BorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dash;
+      series3.BorderWidth = 2;
+      series3.ChartArea = "ChartArea_Y";
+      series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+      series3.Color = System.Drawing.Color.Blue;
+      series3.Legend = "Legend1";
+      series3.LegendText = "Ableitung y\'";
+      series3.Name = "Series_y\'";
+      this.chart_epizykloide.Series.Add(series1);
+      this.chart_epizykloide.Series.Add(series2);
+      this.chart_epizykloide.Series.Add(series3);
+      this.chart_epizykloide.Size = new System.Drawing.Size(446, 810);
+      this.chart_epizykloide.TabIndex = 0;
+      this.chart_epizykloide.Text = "Epizykloide";
+      // 
+      // Form_Epizykloide
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(595, 837);
+      this.Controls.Add(this.splitContainer_epizykloide);
+      this.Controls.Add(this.menuStrip1);
+      this.MainMenuStrip = this.menuStrip1;
+      this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+      this.Name = "Form_Epizykloide";
+      this.Text = "Epizykloide";
+      this.menuStrip1.ResumeLayout(false);
+      this.menuStrip1.PerformLayout();
+      this.splitContainer_epizykloide.Panel1.ResumeLayout(false);
+      this.splitContainer_epizykloide.Panel1.PerformLayout();
+      this.splitContainer_epizykloide.Panel2.ResumeLayout(false);
+      ((System.ComponentModel.ISupportInitialize)(this.splitContainer_epizykloide)).EndInit();
+      this.splitContainer_epizykloide.ResumeLayout(false);
+      ((System.ComponentModel.ISupportInitialize)(this.chart_epizykloide)).EndInit();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.MenuStrip menuStrip1;
+    private System.Windows.Forms.ToolStripMenuItem beendenToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem epizykloideToolStripMenuItem;
+    private System.Windows.Forms.SplitContainer splitContainer_epizykloide;
+    private System.Windows.Forms.DataVisualization.Charting.Chart chart_epizykloide;
+    private System.Windows.Forms.Label label_r_;
+    private System.Windows.Forms.Label label_R;
+    private System.Windows.Forms.TextBox textBox_r_;
+    private System.Windows.Forms.TextBox textBox_R;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.Label label1;
+  }
+}
+
---
+++ 08_DiagrammGrafik/chartEpizykloide/Form_Epizykloide.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace chartEpizykloide
+{
+  public partial class Form_Epizykloide : Form
+  {
+    public Form_Epizykloide()
+    {
+      InitializeComponent();
+    }
+    /// <summary>
+    /// Beendet die Anwendung
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void beendenToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+      Close();
+    }
+/// <summary>
+/// Grafische Darstellung einer Epizykloide, der y-Funktion und deren Ableitung
+/// </summary>
+/// <param name="sender"></param>
+/// <param name="e"></param>
+private void epizykloideToolStripMenuItem_Click(object sender, EventArgs e)
+{
+  //Führungskreisradius R, Abrollkreisradius r
+  double R = Convert.ToDouble(textBox_R.Text);
+  double r_ = Convert.ToDouble(textBox_r_.Text);
+  // Löschen der Vorgängerfunktionen
+  chart_epizykloide.Series[0].Points.Clear();
+  chart_epizykloide.Series[1].Points.Clear();
+  chart_epizykloide.Series[2].Points.Clear();
+  for (int alpha=-180; alpha <= 180; alpha += 10)
+  {
+    double alpha_ = alpha * Math.PI / 180; // Winkel in Bogenmaß
+    double k = 1 + R / r_;  // Radienverhältnis
+    // Koodinaten
+    double x = r_ * Math.Cos(k * alpha_) + (R + r_) * Math.Cos(alpha_);
+    double y = r_ * Math.Sin(k * alpha_) + (R + r_) * Math.Sin(alpha_);
+    // Erste Ableitung von y = y'
+    double  y_ = k * R * Math.Cos(k * alpha_) + (R+r_)* Math.Cos(alpha_);
+    // Funktionen zeichnen
+    chart_epizykloide.Series[0].Points.AddXY(x, y);
+    chart_epizykloide.Series[1].Points.AddXY(alpha, y);
+    chart_epizykloide.Series[2].Points.AddXY(alpha, y_);
+  }
+}
+
+    private void textBox_r__TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    private void label_r__Click(object sender, EventArgs e)
+    {
+
+    }
+  }
+}
