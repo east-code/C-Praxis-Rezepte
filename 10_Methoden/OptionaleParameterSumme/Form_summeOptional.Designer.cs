73875602641def75e05aae33af0b5639c9aee185
Horst-Christian Heinke <98899060+east-code@users.noreply.github.com>
2022-02-21 13:40:57 +0100

    Add files via upload

---
+++ 10_Methoden/OptionaleParameterSumme/Form_summeOptional.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace summeOptional
+{
+    partial class Form_summeOptional
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_OK = new System.Windows.Forms.Button();
+            this.textBox_a = new System.Windows.Forms.TextBox();
+            this.textBox_b = new System.Windows.Forms.TextBox();
+            this.textBox_c = new System.Windows.Forms.TextBox();
+            this.textBox_d = new System.Windows.Forms.TextBox();
+            this.textBox_sum_ab = new System.Windows.Forms.TextBox();
+            this.label_a = new System.Windows.Forms.Label();
+            this.label_b = new System.Windows.Forms.Label();
+            this.label_c = new System.Windows.Forms.Label();
+            this.label_d = new System.Windows.Forms.Label();
+            this.label_sum_ab = new System.Windows.Forms.Label();
+            this.label_sum_abc = new System.Windows.Forms.Label();
+            this.textBox_sum_abc = new System.Windows.Forms.TextBox();
+            this.label_sum_abcd = new System.Windows.Forms.Label();
+            this.textBox_sum_abcd = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button_OK
+            // 
+            this.button_OK.Location = new System.Drawing.Point(187, 15);
+            this.button_OK.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.button_OK.Name = "button_OK";
+            this.button_OK.Size = new System.Drawing.Size(96, 181);
+            this.button_OK.TabIndex = 0;
+            this.button_OK.Text = "OK";
+            this.button_OK.UseVisualStyleBackColor = true;
+            this.button_OK.Click += new System.EventHandler(this.button_OK_Click);
+            // 
+            // textBox_a
+            // 
+            this.textBox_a.BackColor = System.Drawing.Color.LightBlue;
+            this.textBox_a.Location = new System.Drawing.Point(59, 15);
+            this.textBox_a.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.textBox_a.Name = "textBox_a";
+            this.textBox_a.Size = new System.Drawing.Size(107, 31);
+            this.textBox_a.TabIndex = 1;
+            // 
+            // textBox_b
+            // 
+            this.textBox_b.BackColor = System.Drawing.Color.LightBlue;
+            this.textBox_b.Location = new System.Drawing.Point(59, 61);
+            this.textBox_b.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.textBox_b.Name = "textBox_b";
+            this.textBox_b.Size = new System.Drawing.Size(107, 31);
+            this.textBox_b.TabIndex = 2;
+            // 
+            // textBox_c
+            // 
+            this.textBox_c.BackColor = System.Drawing.Color.LightBlue;
+            this.textBox_c.Location = new System.Drawing.Point(59, 109);
+            this.textBox_c.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.textBox_c.Name = "textBox_c";
+            this.textBox_c.Size = new System.Drawing.Size(107, 31);
+            this.textBox_c.TabIndex = 3;
+            // 
+            // textBox_d
+            // 
+            this.textBox_d.BackColor = System.Drawing.Color.LightBlue;
+            this.textBox_d.Location = new System.Drawing.Point(59, 165);
+            this.textBox_d.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.textBox_d.Name = "textBox_d";
+            this.textBox_d.Size = new System.Drawing.Size(107, 31);
+            this.textBox_d.TabIndex = 4;
+            // 
+            // textBox_sum_ab
+            // 
+            this.textBox_sum_ab.BackColor = System.Drawing.Color.Silver;
+            this.textBox_sum_ab.Location = new System.Drawing.Point(506, 61);
+            this.textBox_sum_ab.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.textBox_sum_ab.Name = "textBox_sum_ab";
+            this.textBox_sum_ab.Size = new System.Drawing.Size(135, 31);
+            this.textBox_sum_ab.TabIndex = 5;
+            // 
+            // label_a
+            // 
+            this.label_a.AutoSize = true;
+            this.label_a.Location = new System.Drawing.Point(16, 25);
+            this.label_a.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label_a.Name = "label_a";
+            this.label_a.Size = new System.Drawing.Size(30, 25);
+            this.label_a.TabIndex = 6;
+            this.label_a.Text = "a:";
+            // 
+            // label_b
+            // 
+            this.label_b.AutoSize = true;
+            this.label_b.Location = new System.Drawing.Point(16, 73);
+            this.label_b.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label_b.Name = "label_b";
+            this.label_b.Size = new System.Drawing.Size(30, 25);
+            this.label_b.TabIndex = 7;
+            this.label_b.Text = "b:";
+            // 
+            // label_c
+            // 
+            this.label_c.AutoSize = true;
+            this.label_c.Location = new System.Drawing.Point(18, 115);
+            this.label_c.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label_c.Name = "label_c";
+            this.label_c.Size = new System.Drawing.Size(29, 25);
+            this.label_c.TabIndex = 8;
+            this.label_c.Text = "c:";
+            // 
+            // label_d
+            // 
+            this.label_d.AutoSize = true;
+            this.label_d.Location = new System.Drawing.Point(18, 165);
+            this.label_d.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label_d.Name = "label_d";
+            this.label_d.Size = new System.Drawing.Size(30, 25);
+            this.label_d.TabIndex = 9;
+            this.label_d.Text = "d:";
+            // 
+            // label_sum_ab
+            // 
+            this.label_sum_ab.AutoSize = true;
+            this.label_sum_ab.Location = new System.Drawing.Point(301, 64);
+            this.label_sum_ab.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label_sum_ab.Name = "label_sum_ab";
+            this.label_sum_ab.Size = new System.Drawing.Size(140, 25);
+            this.label_sum_ab.TabIndex = 10;
+            this.label_sum_ab.Text = "Summe(a, b):";
+            // 
+            // label_sum_abc
+            // 
+            this.label_sum_abc.AutoSize = true;
+            this.label_sum_abc.Location = new System.Drawing.Point(301, 115);
+            this.label_sum_abc.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label_sum_abc.Name = "label_sum_abc";
+            this.label_sum_abc.Size = new System.Drawing.Size(163, 25);
+            this.label_sum_abc.TabIndex = 12;
+            this.label_sum_abc.Text = "Summe(a, b, c):";
+            // 
+            // textBox_sum_abc
+            // 
+            this.textBox_sum_abc.BackColor = System.Drawing.Color.Silver;
+            this.textBox_sum_abc.Location = new System.Drawing.Point(506, 109);
+            this.textBox_sum_abc.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.textBox_sum_abc.Name = "textBox_sum_abc";
+            this.textBox_sum_abc.Size = new System.Drawing.Size(135, 31);
+            this.textBox_sum_abc.TabIndex = 11;
+            // 
+            // label_sum_abcd
+            // 
+            this.label_sum_abcd.AutoSize = true;
+            this.label_sum_abcd.Location = new System.Drawing.Point(301, 168);
+            this.label_sum_abcd.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label_sum_abcd.Name = "label_sum_abcd";
+            this.label_sum_abcd.Size = new System.Drawing.Size(193, 25);
+            this.label_sum_abcd.TabIndex = 14;
+            this.label_sum_abcd.Text = "Summe(a, b, c , d):";
+            // 
+            // textBox_sum_abcd
+            // 
+            this.textBox_sum_abcd.BackColor = System.Drawing.Color.Silver;
+            this.textBox_sum_abcd.Location = new System.Drawing.Point(506, 162);
+            this.textBox_sum_abcd.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.textBox_sum_abcd.Name = "textBox_sum_abcd";
+            this.textBox_sum_abcd.Size = new System.Drawing.Size(135, 31);
+            this.textBox_sum_abcd.TabIndex = 13;
+            // 
+            // Form_summeOptional
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(657, 216);
+            this.Controls.Add(this.label_sum_abcd);
+            this.Controls.Add(this.textBox_sum_abcd);
+            this.Controls.Add(this.label_sum_abc);
+            this.Controls.Add(this.textBox_sum_abc);
+            this.Controls.Add(this.label_sum_ab);
+            this.Controls.Add(this.label_d);
+            this.Controls.Add(this.label_c);
+            this.Controls.Add(this.label_b);
+            this.Controls.Add(this.label_a);
+            this.Controls.Add(this.textBox_sum_ab);
+            this.Controls.Add(this.textBox_d);
+            this.Controls.Add(this.textBox_c);
+            this.Controls.Add(this.textBox_b);
+            this.Controls.Add(this.textBox_a);
+            this.Controls.Add(this.button_OK);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form_summeOptional";
+            this.Text = "Summe, optional";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_OK;
+        private System.Windows.Forms.TextBox textBox_a;
+        private System.Windows.Forms.TextBox textBox_b;
+        private System.Windows.Forms.TextBox textBox_c;
+        private System.Windows.Forms.TextBox textBox_d;
+        private System.Windows.Forms.TextBox textBox_sum_ab;
+        private System.Windows.Forms.Label label_a;
+        private System.Windows.Forms.Label label_b;
+        private System.Windows.Forms.Label label_c;
+        private System.Windows.Forms.Label label_d;
+        private System.Windows.Forms.Label label_sum_ab;
+        private System.Windows.Forms.Label label_sum_abc;
+        private System.Windows.Forms.TextBox textBox_sum_abc;
+        private System.Windows.Forms.Label label_sum_abcd;
+        private System.Windows.Forms.TextBox textBox_sum_abcd;
+    }
+}
+
---
+++ 10_Methoden/OptionaleParameterSumme/Form_summeOptional.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace summeOptional
+{
+    public partial class Form_summeOptional : Form
+    {
+        public Form_summeOptional()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Nutzerschnittstelle
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_OK_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(textBox_a.Text);
+            double b = Convert.ToDouble(textBox_b.Text);
+            double c = Convert.ToDouble(textBox_c.Text);
+            double d = Convert.ToDouble(textBox_d.Text);
+            //Aufruf einer Methode mit optinalen Parametern
+            textBox_sum_ab.Text = summeOptional(a, b).ToString(); 
+            textBox_sum_abc.Text = summeOptional(a, b, c).ToString();
+            textBox_sum_abcd.Text = summeOptional(a, b, c, d).ToString();
+        }
+
+        /// <summary>
+        /// Summe von maximal vier Zahlen
+        /// Zahlen X2, x3, x4 sin optinal
+        /// </summary>
+        /// <param name="x1"></param>
+        /// <param name="x2"></param>
+        /// <param name="x3"></param>
+        /// <param name="x4"></param>
+        /// <returns></returns>
+        private double summeOptional(double x1, double x2=0, double x3=0, double x4=0)
+        {
+        return x1 + x2 + x3 + x4;
+        }
+
+
+
+
+    }
+}
